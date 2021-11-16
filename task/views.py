7fca1934c6f327d86dc23ad790265861f683606e
Vjet Node JS <hoaboy89@gmail.com>
2021-11-16 15:40:36 +0700

    Complete product

--- task/views.py
+++ task/views.py
@@ -1,10 +1,28 @@
+from os import name
 from django.shortcuts import render
 from django.http import HttpResponse
 import task
-# Create your views here.
-tasks= ["Check email", 'Reading book','Testing']
+from django import forms
+from django.http.response import HttpResponseRedirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+class AddTaskForm(forms.Form):
+    task = forms.CharField(label='Task Name')
+
 def index(request):
-    return render(request, 'task/index.html', {'tasks': tasks})
+    if 'tasks' not in request.session:
+        request.session['tasks'] = []
+    return render(request, 'task/index.html', {'tasks': request.session['tasks']})
 def add(request):
-    return render(request, 'task/add.html')
+    if request.method == 'POST':
+        form= AddTaskForm(request.POST)
+        if form.is_valid():
+            task= form.cleaned_data['task']
+            request.session['tasks']+=[task]
+            return HttpResponseRedirect(reverse('task:index'))
+        else:
+            return render(request, 'task/add.html', {'form': form})
+    return render(request, 'task/add.html',{
+        'form': AddTaskForm()
+    })
     
